ea0bb4d386372a8bc41afe228a6c50484813ebc1
davidomahony <daverock914@gmail.com>
2022-12-20 20:15:07 +0000

    Infra change

--- infra/AppService/main.tf
+++ infra/AppService/main.tf
@@ -3,18 +3,19 @@ data "azurerm_resources" "rg-movie-demo" {
   resource_group_name = "rg-movie-demo"
 }
 
-resource "azurerm_app_service_plan" "asp-movie-demo" {
+resource "azurerm_service_plan" "asp-movie-demo" {
   name                = "asp-movie-demo"
   location            = "northeurope"
   resource_group_name = "rg-movie-demo"
   os_type             = "Linux"
-  sku_name            = "B1"
+  sku_name            = "F1"
 }
 
 resource "azurerm_linux_web_app" "example" {
   name                = "as-movie-demo"
-  location            = "northeurope"
   resource_group_name = "rg-movie-demo"
-  app_service_plan_id = azurerm_app_service_plan.asp-movie-demo.id
+  location            = azurerm_service_plan.asp-movie-demo.location
+  service_plan_id     = azurerm_service_plan.asp-movie-demo.id
+
   site_config {}
 }
