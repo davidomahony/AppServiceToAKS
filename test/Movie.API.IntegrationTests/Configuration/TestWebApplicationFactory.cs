bcdc6616f3f077d3ff3d00c46d18e639a15ed292
davidomahony <daverock914@gmail.com>
2022-12-21 14:38:13 +0000

    Adding dev integration tests

---
+++ test/Movie.API.IntegrationTests/Configuration/TestWebApplicationFactory.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movie.API.IntegrationTests.Configuration
+{
+    public class TestWebApplicationFactory : WebApplicationFactory<Program> 
+    {
+    }
+}
