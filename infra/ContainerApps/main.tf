6375665b281a7fe77602448e6d720975cef47911
davidomahony <daverock914@gmail.com>
2022-12-22 11:44:08 +0000

    Adding container instacne

--- infra/ContainerApps/main.tf
+++ infra/ContainerApps/main.tf
@@ -14,4 +14,29 @@ resource "azurerm_container_registry" "movie-demo-registry" {
   admin_enabled         = false
 }
 
+resource "azurerm_container_group" "movie-demo-container-instance" {
+  name                  = "movie-demo-container-instance"
+  location              = "${data.azurerm_resource_group.rg-movie-demo.location}"
+  resource_group_name   = "${data.azurerm_resource_group.rg-movie-demo.name}"
+  ip_address_type       = "Public"
+  dns_name_label        = "movie-demo-container-instance"
+  os_type               = "Linux"
+
+  container {
+    name   = "movie-demo-container"
+    image  = "mcr.microsoft.com/azuredocs/aci-helloworld:latest"
+    cpu    = "0.5"
+    memory = "1.5"
+
+    ports {
+      port     = 443
+      protocol = "TCP"
+    }
+  }
+
+  tags = {
+    environment = "testing"
+  }
+}
+
 // May need to give different container groups an identity with access to registry
