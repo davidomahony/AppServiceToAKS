46d811578ed1d66e056c5f4fd56e4515cebccfc0
davidomahony <daverock914@gmail.com>
2022-12-22 11:37:58 +0000

    Removing underscores

--- infra/ContainerApps/main.tf
+++ infra/ContainerApps/main.tf
@@ -7,7 +7,7 @@ data "azurerm_resource_group" "rg-movie-demo" {
 }
 
 resource "azurerm_container_registry" "movie-demo-registry" {
-  name                  = "movie-demo-registry"
+  name                  = "moviedemoregistry"
   location              = "${data.azurerm_resource_group.rg-movie-demo.location}"
   resource_group_name   = "${data.azurerm_resource_group.rg-movie-demo.name}"
   sku                   = "Basic"
