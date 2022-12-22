30939623e34dac0b53adbbf8725473ecc1ff5f23
davidomahony <daverock914@gmail.com>
2022-12-22 11:36:37 +0000

    Updating

--- infra/ContainerApps/main.tf
+++ infra/ContainerApps/main.tf
@@ -7,11 +7,11 @@ data "azurerm_resource_group" "rg-movie-demo" {
 }
 
 resource "azurerm_container_registry" "movie-demo-registry" {
-  name                = "movie-demo-registry"
-  resource_group_name = azurerm_resource_group.rg-movie-demo.name
-  location            = azurerm_resource_group.rg-movie-demo.location
-  sku                 = "Basic"
-  admin_enabled       = false
+  name                  = "movie-demo-registry"
+  location              = "${data.azurerm_resource_group.rg-movie-demo.location}"
+  resource_group_name   = "${data.azurerm_resource_group.rg-movie-demo.name}"
+  sku                   = "Basic"
+  admin_enabled         = false
 }
 
 // May need to give different container groups an identity with access to registry
